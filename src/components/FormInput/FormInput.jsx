e1100bb29100b892fe2b12ca8f8077d08cb16b72
arseniy-grinev <arseniy.grinev@gmail.com>
2022-10-20 18:23:44 +0300

    use Formik

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormLabel, Input } from './FormInput.styled';
+import styled from 'styled-components';
+import { Field, ErrorMessage } from 'formik';
+import { FormLabel } from './FormInput.styled';
 
-const FormInput = ({ type, name, label, value, onChange, pattern, title }) => {
+const Input = styled(Field)`
+  border: 1px solid lightgray;
+  margin: 20px;
+  margin-top: 5px;
+  font-size: 16px;
+  width: 360px;
+  outline: none;
+  border-radius: 2px;
+  &:hover,
+  &:focus,
+  &:active {
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.2),
+      0px 1px 1px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const FormInput = ({ type, name, label, title }) => {
   return (
-    <FormLabel>
+    <FormLabel htmlFor={name}>
       {label}
-      <Input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        pattern={pattern}
-        title={title}
-        required
-      />
+      <Input type={type} name={name} />
+      <ErrorMessage name={name} component="div">
+        {title}
+      </ErrorMessage>
     </FormLabel>
   );
 };
@@ -23,10 +36,7 @@ FormInput.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
-  pattern: PropTypes.string.isRequired,
 };
 
 export default FormInput;
